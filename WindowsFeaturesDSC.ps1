c841e38218781979a6d65403d2bd57662aa84bd3
Tim Curwick <MadWithPowerShell@gmail.com>
2017-06-20 10:43:48 -0500

    Added WindowsFeatures

---
+++ WindowsFeaturesDSC.ps1
@@ -0,0 +1,33 @@
+﻿<#
+.SYNOPSIS
+    WindowsFeaturesDSC.ps1
+
+.Description
+    DSC configuration to install arbitrary list of Windows features
+
+.NOTES
+    Designed to be used by "RBA Azure VM build console.ps1"
+
+.NOTES
+    v 1.0   6/20/2017  Tim Curwick  Created
+#>
+
+configuration WindowsFeatures
+    {
+    param
+        (
+        [String]$FeatureList
+        )
+
+    Node localhost
+        {
+        ForEach ( $Feature in $FeatureList.Split( ',;' ) )
+            {
+            WindowsFeature $Feature
+                {
+                Name   = $Feature
+                Ensure = "Present"
+                }
+            }
+        }
+    }
